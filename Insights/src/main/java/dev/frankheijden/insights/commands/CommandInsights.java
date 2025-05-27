e618ef37e0c1325218d98b4c9562c7908c9dc328
James Lyne <jim+github@not-null.co.uk>
2025-05-27 23:38:56 +0100

    Remove info command

--- Insights/src/main/java/dev/frankheijden/insights/commands/CommandInsights.java
+++ Insights/src/main/java/dev/frankheijden/insights/commands/CommandInsights.java
@@ -4,7 +4,6 @@ import dev.frankheijden.insights.Insights;
 import dev.frankheijden.insights.api.InsightsPlugin;
 import dev.frankheijden.insights.api.commands.InsightsCommand;
 import dev.frankheijden.insights.api.config.Messages;
-import dev.frankheijden.insights.api.utils.ColorUtils;
 import dev.frankheijden.insights.api.utils.StringUtils;
 import dev.frankheijden.insights.concurrent.ContainerExecutorService;
 import org.bukkit.command.CommandSender;
@@ -16,19 +15,6 @@ public class CommandInsights extends InsightsCommand {
 
     public CommandInsights(InsightsPlugin plugin) {
         super(plugin);
-    }
-
-    @Command("")
-    @Permission("insights.info")
-    private void showBase(CommandSender sender) {
-        sender.sendMessage(ColorUtils.colorize(
-                "&8&l&m---------------=&r&8[ &b&lInsights&8 ]&l&m=----------------",
-                "&b Plugin version: &a" + plugin.getDescription().getVersion(),
-                "&b Plugin author: &7FrankHeijden#0099",
-                "&b Plugin link: &7https://www.spigotmc.org/resources/56489/",
-                "&b Wiki: &7https://github.com/InsightsPlugin/Insights/wiki",
-                "&8&m-------------------------------------------------"
-        ));
     }
 
     @Command("reload")
