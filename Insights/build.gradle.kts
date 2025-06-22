48cc4896c8c5d10399c544c50bb78ab37c31cd8c
James Lyne <jim+github@not-null.co.uk>
2025-06-22 15:58:40 +0100

    Fix api-version

--- Insights/build.gradle.kts
+++ Insights/build.gradle.kts
@@ -30,7 +30,7 @@ tasks.withType<ShadowJar> {
 paper {
     main = "dev.frankheijden.insights.Insights"
     description = "Insights about your server and regional block limits"
-    apiVersion = "1.21.5"
+    apiVersion = "1.21.6"
     website = "https://github.com/InsightsPlugin/Insights"
     authors = listOf("FrankHeijden")
     serverDependencies {
